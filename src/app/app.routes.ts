1d9112cd9e3f58c52b9841cfb1d378a0637afe0e
theQuery <tomkri2000@outlook.com>
2024-03-03 23:37:25 +0100

    Add Home, alter Navbar style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 import { VideoPageComponent } from './video-page/video-page.component';
 
 export const routes: Routes = [
+    { path: 'hjem', component: HomePageComponent },
     { path: 'video', component: VideoPageComponent },
-    { path: 'hjem', component: VideoPageComponent },
     { path: '', redirectTo: '/video', pathMatch: 'full' },
     { path: '**', redirectTo: '/video', pathMatch: 'full' },
 ];
